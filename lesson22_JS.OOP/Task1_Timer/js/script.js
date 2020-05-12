0ac2bc117fa7773373b4982573d11dca1a179b79
Bohdan <bohdanchoniy@gmail.com>
2020-05-12 19:51:01 +0300

    lesson 23

--- lesson22_JS.OOP/Task1_Timer/js/script.js
+++ lesson22_JS.OOP/Task1_Timer/js/script.js
@@ -1,5 +1,18 @@
-class Timer{
-    start(){
-        
+class Timer {
+    start() {
+        let time = prompt("Input timer time, s:"), i = 1;
+        let snd = new Audio("sound/beep.wav");
+        var timerId = setInterval(() => $(".time").html(i++), 1000);
+        setTimeout(() => { clearInterval(timerId); snd.play(); }, 1000 * time);
+    }
+    stop() {
+        clearInterval(timerId);
     }
 }
+let timer = new Timer;
+$(".start").on("click", function () {
+    timer.start();
+});
+$(".stop").on("click", function () {
+    timer.stop();
+});
---
+++ lesson23_JS.ES6+/lesson/js/script.js
@@ -0,0 +1,14 @@
+// let getRemainder = (firstNumber, secondNumber = 2) => firstNumber % secondNumber;
+// console.log(getRemainder(5));
+// let cars = new Map([
+//     ["Mercedes", 1],
+//     ["BMW", 2],
+//     ["VolksWagen", 3]
+// ]);
+// let getData = (map) => {
+//     for (let item of cars) {
+//         console.log(item);
+//     }
+// };
+// getData(cars);
+alert(2**53);
